4cace87db1303c7b7ff292956897ffe96a9e66fb
priyanshushah685-beep <priyanshushah685@gmail.com>
2026-07-02 19:10:24 +0530

    Week 1 Day 1 - Assignment completed

---
+++ Day1_assignment.py
@@ -0,0 +1,19 @@
+# ASSIGNMENT: Create a program about yourself
+# Requirements:
+# 1. Store your name in a variable
+# 2. Store your age in a variable
+# 3. Store your favorite hobby in a variable
+# 4. Print them using f-strings
+# 5. Calculate your birth year (current_year - age)
+# 6. Print the birth year
+
+name = "Priyanshu shah"
+age = 17
+hobby = "coding"
+print(f"My name is {name}")
+print(f"I am {age} years old")
+print(f"My favourite hobby is {hobby}")
+
+current_year = 2026
+birth_year = (current_year - age)
+print(f"I was born in {birth_year}")
